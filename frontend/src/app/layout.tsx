6c345669b534c7f971e26bf7be69778cbb6778f6
Kasso4ka <serovdn22@gmail.com>
2025-10-25 14:40:10 +0300

    chore(app): initialize Next.js project with required dependencies

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "M3N747",
+  description: "M3N747",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen">
+      <h1>M3N747</h1>
+    </div>
+  );
+}
